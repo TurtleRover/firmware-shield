4b766073f475e8b0ebb9309ee852bbb2a08fbe63
Marcin Twardak <twardakm@gmail.com>
2017-07-18 14:49:09 +0200

    added mani constants

--- eclipse/include/motors.h
+++ eclipse/include/motors.h
@@ -4,10 +4,10 @@
 #include "stm32f0xx_hal.h"
 #include "main.h"
 
-#define	MANI_AXIS_1_MIN	1000 //2800
-#define MANI_AXIS_1_MAX	5000 //3900
-#define MANI_AXIS_2_MIN	1000 //3500
-#define MANI_AXIS_2_MAX	5000 //4200
+#define	MANI_AXIS_1_MIN	500 //2800
+#define MANI_AXIS_1_MAX	6000 //3900
+#define MANI_AXIS_2_MIN	500 //3500
+#define MANI_AXIS_2_MAX	6000 //4200
 #define MANI_GRIPPER_MIN 1000
 #define MANI_GRIPPER_MAX 5000
 #define MANI_DIFF 5	//the lower number the slower manipulator
